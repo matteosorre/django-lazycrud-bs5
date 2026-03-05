1c7b279dc311df58807e3328a961c1769da84e83
Matteo Sorrentino <matteosorre@msn.com>
2026-03-06 00:19:10 +0100

    modifico icone e grafica object_detail

--- lazycrud/static/lazycrud/js/object_list.js
+++ lazycrud/static/lazycrud/js/object_list.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.addEventListener('mousedown', function (e) {
         var row = e.target.closest('.clickable_row');
         if (!row) return;
+        if (e.target.closest('a, button')) return;  // permette di cliccare su buttons sulla riga
         var url = row.dataset.url;
         if (!url) return;
         if (row.dataset.target === '_blank' || e.button === 2) {
